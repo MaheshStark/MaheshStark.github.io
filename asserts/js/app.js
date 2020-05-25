07933aaa0c4f2bcc342cf766cd70b354641204ef
MaheshStark <mbkmahesh1996@gmail.com>
2020-05-25 15:56:53 +0530

    navbar

---
+++ asserts/js/app.js
@@ -0,0 +1,17 @@
+$(document).ready(function(){
+    $(window).scroll(function(){
+        var myNav = document.getElementById('mynav');
+        if($(window).scrollTop() > 250){
+            myNav.classList.add("scrolling");   
+            $('.nav-link').css('color', '#fff');         
+        }
+        else{
+            myNav.classList.remove("scrolling");
+            $('.nav-link').css('color', 'red');
+        }
+
+    });
+    $('#btnToggler').click(function(){
+        $(".mobile-menu").fadeToggle("slow");   
+      });
+})
