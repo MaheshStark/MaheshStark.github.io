8f4639921c99fa40cd537ab8823c3f5bc26bc3d4
MaheshStark <mbkmahesh1996@gmail.com>
2020-05-26 23:25:12 +0530

    scratch completed

--- asserts/js/app.js
+++ asserts/js/app.js
@@ -1,13 +1,18 @@
 $(document).ready(function(){
     $(window).scroll(function(){
         var myNav = document.getElementById('mynav');
+        var upSorller = document.getElementById('arrowup');
         if($(window).scrollTop() > 250){
             myNav.classList.add("scrolling");   
-            $('.nav-link').css('color', '#fff');         
+            $('.nav-link').css('color', '#fff');
+            upSorller.classList.add("show"); 
+            upSorller.classList.remove("not-show");         
         }
         else{
             myNav.classList.remove("scrolling");
             $('.nav-link').css('color', '#ffffff');
+            upSorller.classList.add("not-show"); 
+            upSorller.classList.remove("show");  
         }
 
     });
