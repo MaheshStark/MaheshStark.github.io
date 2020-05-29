fba594df3c58d702671e6d6d4915718855774df9
MaheshStark <mbkmahesh1996@gmail.com>
2020-05-29 22:20:17 +0530

    Smouth scorling

--- asserts/js/app.js
+++ asserts/js/app.js
@@ -19,6 +19,27 @@ $(document).ready(function(){
     $('#btnToggler').click(function(){
         $(".mobile-menu").fadeToggle("slow");   
       });
+      $("a").on('click', function(event) {
+
+        // Make sure this.hash has a value before overriding default behavior
+        if (this.hash !== "") {
+          // Prevent default anchor click behavior
+          event.preventDefault();
+    
+          // Store hash
+          var hash = this.hash;
+    
+          // Using jQuery's animate() method to add smooth page scroll
+          // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+          $('html, body').animate({
+            scrollTop: $(hash).offset().top
+          }, 800, function(){
+    
+            // Add hash (#) to URL when done scrolling (default click behavior)
+            window.location.hash = hash;
+          });
+        } // End if
+      });
 })
 
 function displayProj(image,name){
